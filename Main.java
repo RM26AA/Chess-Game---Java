fdfc9ff7f3dba17486e7521869afb3dd412485c2
Romeo Maunick <rmaunick26@gmail.com>
2025-04-23 13:06:34 +0100

    Game files

---
+++ Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import main.Board;
+
+import javax.swing.*;
+import java.awt.*;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(Color.black);
+        frame.setLayout(new GridBagLayout());
+        frame.setMinimumSize(new Dimension(1000, 1000));
+        frame.setLocationRelativeTo(null);
+
+        Board board = new Board();
+        frame.add(board);
+
+
+        frame.setVisible(true);
+    }
+}
